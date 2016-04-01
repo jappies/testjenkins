0a3dd9a90d54ac915f918b44f6408f60b49e45c3
jasper <jasper_ssx@hotmail.com>
2016-04-01 12:12:25 +0200

    added little doxygen for autogenerate

--- source/include/Addition.hpp
+++ source/include/Addition.hpp
@@ -1,5 +1,11 @@
 #ifndef _ADDITION_HPP_
 #define _ADDITION_HPP_
+
+/// Class Addition
+/// This class addes things.
+/// \authors Jasper van hulst
+/// \version 1.0
+/// \date 1-4-2016
 class Addition {
 public:
     static int twoValues(const int x, const int y);   
